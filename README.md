306cae496441c23fe3e9207e55402fa1e5b5ac4a
arnaupv <arnau.pont.vilchez@gmail.com>
2019-02-20 13:46:24 +0100

    avgCyclicDir-0.9.0: Operative Version

---
+++ README.md
@@ -0,0 +1,36 @@
+# OpenFOAM post-processing utility: avgCyclicDir
+This OpenFOAM post-processing utility is designed for carrying out a spatial averaging in the cyclic direction (if any). The averaged data is saved using a new mesh with a unique control volume in the cyclic direction, considerably reducing the required amount of memory.
+
+**Instalation**
+* Use ./Allwmake
+
+**Capabilities**
+* Any cyclic direction should be allowed (Please report any possible issue). 
+
+**How to use ?**
+
+* The only argument required by this tool is the *interpolationScheme*. 
+* It has to be executed in the main OpenFOAM folder.
+  For instance: 
+  	avgCyclicDir cellPoint
+
+* The tool only postprocess time averaged fields (Following extension: "\*Mean").
+* Once executed, a folder located at "*./postProcessing/avgCyclicDir*" is created containing the extruded mesh with the interpolated fields.
+* For visualizing purposes, a symbolic link for the system folder is required by paraFOAM. Execute the following commands from the OpenFOAM project folder: 
+    cd ./postProcessing/avgCyclicDir
+	ln -s ../../system
+
+**Limitiations :**
+
+* OpenFOAM v1706 [Other OpenFOAM versions have not been tested].
+* This tool is not yet paralellized.
+* Only one cyclic direction is allowed.
+* The spatial discretitzation in the cyclic direction should be uniform. Otherwise
+the tool will not work, or will provide wrong results.
+* Currently, all patch values are set to zero.
+
+**Furtherwork :**
+
+* Tool parallelitzation
+* Fix patch zero values
+* Mulitple cyclic directions.
---
+++ avgCyclicDir.C
@@ -0,0 +1,94 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    averaging in the spanwise direction z
+
+
+\*---------------------------------------------------------------------------*/
+
+#include "argList.H"
+#include "polyMesh.H"
+#include "extrude2DMesh.H"
+#include "extrudeModel.H"
+#include "polyTopoChange.H"
+#include "MeshedSurface.H"
+#include "fvCFD.H"
+#include "cyclicPolyPatch.H"
+#include "sampledPlane.H"
+#include "vtkSurfaceWriter.H"
+#include "edgeCollapser.H"
+#include "addPatchCellLayer.H"
+#include "patchToPoly2DMesh.H"
+#include "wordRe.H"
+#include "wordRes.H"
+#include "ReadFields.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+fileName avgCyclicDirFolder = "../postProcessing/avgCyclicDir/";
+
+#include "functions.H"
+
+// -------------INPUT PARAMETERS ---------------------
+
+//cyclic patch ID from which averaging is performed
+// -------------INPUT PARAMETERS ---------------------
+
+int main(int argc, char *argv[])
+{
+	argList::addNote
+	(
+		"Enjoy the spatial averaging in the cylic direction!"
+	); 
+
+	argList::noParallel();
+
+	#include "addRegionOption.H"
+
+	argList::validArgs.append("interpolationScheme");
+
+	#include "setRootCase.H"
+    #include "createTime.H"
+	// Allow override of time 
+    //instantList timeDirs = timeSelector::select0(runTime, args);
+	instantList times = timeSelector::selectIfPresent(runTime, args);
+	Info << times << endl;
+
+   	//Arguments
+	word interpolationScheme 			= args[1];
+
+	#include "createMesh.H"
+	
+	#include "getDataFromCyclicPatches.H"
+	#include "createExtrudedMesh.H"
+	#include "readFields.H"
+	#include "extrudeFields.H"
+ 
+	Info<< "End" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
